ae7f0ff5078eab3c583f3e9fc907351b2d93888a
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-06 16:47:20 +0000

    Setup init wonka thingy doing classes for now

--- js/wonka.js
+++ js/wonka.js
@@ -1,4 +1,30 @@
-define([], function() {
+define(['onLoad', 'global'], function(onLoad, global) {
   'use strict';
-  console.log('Wonka!');
+  global.attribute.set('color', 'yellow');
+
+  onLoad(function(){
+    // domReady
+    $('html').removeClass('loading');
+    $('html').addClass('post-load');
+
+    $(document).trigger('loaded');
+
+    // TODO: Use transitionend
+    // $(selector).on('transitionend', function(){});
+    setTimeout(function(){
+      $('html').removeClass('post-load');
+      $('html').addClass('loaded');
+    },500);
+  }, function(){
+    // re-loading
+    $('html').addClass('re-loading');
+    $(document).trigger('re-loaded');
+
+    // TODO: Use transitionend
+    // $(selector).on('transitionend', function(){});
+    setTimeout(function(){
+      $('html').removeClass('re-loading');
+      $('html').addClass('re-loaded');
+    },500);
+  });
 });
