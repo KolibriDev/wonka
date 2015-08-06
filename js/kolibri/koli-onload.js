35f74e0b99a66da7d83455f9599bf064aa532650
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-08-06 14:13:16 +0000

    Remove current javascript, start new minimal base

--- js/kolibri/koli-onload.js
+++
@@ -1,8 +0,0 @@
-define(['domReady', 'jquery'], function(domReady, $){
-  'use strict';
-  var onLoad = function(callback) {
-    domReady(callback);
-    $(document).on('re-loaded', callback);
-  };
-  return onLoad;
-});
--- js/main.js
+++ js/main.js
@@ -1,49 +1,21 @@
-
 (function() {
   require.config({
     baseUrl: '/js',
-    urlArgs: 'nocache=' + (new Date()).getTime(),
     paths: {
+      zepto: 'vendor/zepto-build',
+      promise: 'vendor/promise',
       domReady: 'vendor/domReady',
-      classList: 'vendor/classList',
-      jquery: 'vendor/jquery.min',
-      jQeasing: 'vendor/jquery.easing.min',
-      underscore: 'vendor/underscore-min',
-      history: 'vendor/jquery.history',
-      browser: 'vendor/browser',
       modernizr: 'vendor/modernizr',
 
-      rafPolyfill: 'kolibri/raf-polyfill',
-      scrolltrigger: 'kolibri/koli-scrolltrigger',
-      smoothscroll: 'kolibri/koli-smoothscroll',
-      navigate: 'kolibri/koli-navigate',
-      navCtrl: 'kolibri/koli-nav-ctrl',
-      cssTranslate: 'kolibri/koli-css-translate',
-      onLoad: 'kolibri/koli-onload',
-
-      align: 'kolibri/koli-align',
-      alignHelpers: 'kolibri/koli-align-helpers',
-      alignEqualize: 'kolibri/koli-align-equalize',
-      alignVerticalCenter: 'kolibri/koli-align-vertical-center',
-      alignWindowHeight: 'kolibri/koli-align-window-height',
-
-      viewmodel: 'viewmodel',
+      onLoad: 'vendor/koli-onload',
     },
     shim: {
-      jquery: { exports: '$' },
-      jQeasing: { deps: ['jquery'] },
-      underscore: { exports: '_' },
-      history: { deps: ['jquery'], exports: 'History' },
       modernizr: { exports: 'Modernizr' },
+      zepto: { exports: 'Zepto' },
     }
   });
 
   require([
-    'init/viewmodel',
-    'init/navigation',
-    'init/align',
-    'init/smoothscroll',
-    'init/scrolltrigger',
-    'init/parallax',
+    'wonka'
   ]);
 }).call(this);
